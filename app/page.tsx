6456a392d578301b7a3b65d966bd3e422606c438
Michael Jensen <mcjensen24@gmail.com>
2025-07-23 11:14:26 -0400

    removed reddit post and added reddit links up top

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 
 export default function Home() {
   // Load Twitter and Instagram widgets scripts
+// Load Twitter, Instagram, and Reddit widgets scripts
   useEffect(() => {
     // Check if script is already loaded to avoid duplicates
     if (!document.querySelector('script[src="https://platform.twitter.com/widgets.js"]')) {
@@ -21,6 +22,15 @@ export default function Home() {
       instagramScript.src = '//www.instagram.com/embed.js'
       instagramScript.async = true
       document.head.appendChild(instagramScript)
+    }
+
+    // Load Reddit embed script
+    if (!document.querySelector('script[src="https://embed.reddit.com/widgets.js"]')) {
+      const redditScript = document.createElement('script')
+      redditScript.src = 'https://embed.reddit.com/widgets.js'
+      redditScript.async = true
+      redditScript.charset = 'UTF-8'
+      document.head.appendChild(redditScript)
     }
   }, [])
 
@@ -64,6 +74,16 @@ export default function Home() {
       className: 'social-twitter'
     },
     { 
+      name: 'Reddit', 
+      icon: (
+        <svg viewBox="0 0 16 16" fill="white">
+          <path d="M13.01 1.744c.688 0 1.25.561 1.25 1.249a1.25 1.25 0 0 1-2.498.056l-2.597-.547-.8 3.747c1.824.07 3.48.632 4.674 1.488.308-.309.73-.491 1.207-.491.968 0 1.754.786 1.754 1.754 0 .716-.435 1.333-1.01 1.614a3.111 3.111 0 0 1 .042.52c0 2.694-3.13 4.87-7.004 4.87-3.874 0-7.004-2.176-7.004-4.87 0-.183.015-.366.043-.534A1.748 1.748 0 0 1 .028 9c0-.968.786-1.754 1.754-1.754.463 0 .898.196 1.207.49 1.207-.883 2.878-1.43 4.744-1.487l.885-4.182a.342.342 0 0 1 .14-.197.35.35 0 0 1 .238-.042l2.906.617a1.214 1.214 0 0 1 1.108-.701zM5.25 9C4.561 9 4 9.562 4 10.25c0 .687.561 1.248 1.25 1.248.687 0 1.248-.561 1.248-1.249 0-.688-.561-1.249-1.249-1.249zm5.5 0c-.687 0-1.248.561-1.248 1.25 0 .687.561 1.248 1.249 1.248.688 0 1.249-.561 1.249-1.249 0-.687-.562-1.249-1.25-1.249zm-5.466 3.99a.327.327 0 0 0-.231.094.33.33 0 0 0 0 .463c.842.842 2.484.913 2.961.913.477 0 2.105-.056 2.961-.913a.361.361 0 0 0 .029-.463.33.33 0 0 0-.464 0c-.547.533-1.684.73-2.512.73-.828 0-1.979-.196-2.512-.73a.326.326 0 0 0-.232-.095z"/>
+        </svg>
+      ), 
+      url: 'https://www.reddit.com/user/Brooklynxrose/',
+      className: 'social-reddit'
+    },
+    { 
       name: 'YouTube', 
       icon: (
         <svg viewBox="0 0 24 24" fill="currentColor">
@@ -99,6 +119,11 @@ export default function Home() {
     { 
       title: "Twitter/X", 
       url: "https://x.com/brooklynrose869",
+      className: "link-button"
+    },
+    { 
+      title: "Reddit", 
+      url: "https://www.reddit.com/user/Brooklynxrose/",
       className: "link-button"
     },
     { 
@@ -261,7 +286,8 @@ export default function Home() {
         </blockquote>
       </div>
 
-      {/* Twitter Section 
+      {/* Twitter Section       
+
       <div className="twitter-section">
         <blockquote className="twitter-tweet">
           <p lang="zxx" dir="ltr">
@@ -271,6 +297,15 @@ export default function Home() {
         </blockquote>
       </div>
       */}
+
+      {/* Footer */}
+      <div className="footer">
+        <div className="footer-content">
+          <p>© 2025 Brooklyn Rose. All rights reserved.</p>
+        </div>
+      </div>
     </div>
+
+    
   )
 }
